be210fded3799ffe41c76a1d055e979f9d1dfaf0
ergiam <ergiadimaulana@gmail.com>
2021-10-07 14:56:06 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+class contoh01 extends CI_controller
+{
+public function index ()
+{
+    echo"<hl>perkenalan</h1>";
+    echo"Nama saya ERGI ADI MAULANA
+         saya tinggal di daerah Bojonggambir
+         hobby saya adalah Olahraga
+         no telp 085288755629":
+         }
+    }
+?>
